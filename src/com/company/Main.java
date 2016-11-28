aa3d77b817ba62bfdb1f4e6c6acad057e3044c1e
sniperkattenskol <te15.nils.lindfors@umea.nti.se>
2016-11-28 11:26:07 +0100

    Uppgfit 5.5 klar

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
+//Nils.E.L 2016
+
 package com.company;
 
+
+import javax.swing.*;
+
 public class Main {
+    //sida 122, uppgift 5.8
+    public static void del1() {
+
+        String rader = JOptionPane.showInputDialog(null,"Hur många rader? (endast hel tal)"); //Läser in antal rader man vill ha
+        int antalrader = Integer.parseInt(rader);                                                                    //konveterar stringen till en int
+        int test = 0;
+        String displaystring = "";
+
+        for(int i = 1; i <= antalrader; i++){
+            //går igenom alla rader
+            for(int n = 1; n<=i; n++) {
+                //tar raden gånger i om i är mindre är lika med raden
+                int displaynumbs = i*n;
+                //lägger till raden i stringen
+                displaystring = displaystring + Integer.toString(displaynumbs) + " ";
+            }
+
+            displaystring = displaystring + "\n";  //radbyte
+
+        }
+        JOptionPane.showMessageDialog(null, displaystring);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        del1();
+
     }
 }
