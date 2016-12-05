cd4eb9ed777b64c674025b5ab7f10da972a1aeab
lini15 <te15.nils.lindfors@umea.nti.se>
2016-12-05 09:35:47 +0100

    5.7 är klar, fick hjälp av jonas.
    Tips: Läs uppgiften ordentligt, riktigt ordentligt.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,16 @@ public class Main {
     //sida 122, uppgift 5.8
     public static void del1() {
 
-        String rader = JOptionPane.showInputDialog(null,"Hur många rader? (endast hel tal)"); //Läser in antal rader man vill ha
+        String rader = JOptionPane.showInputDialog(null, "Hur många rader? (endast hel tal)"); //Läser in antal rader man vill ha
         int antalrader = Integer.parseInt(rader);                                                                    //konveterar stringen till en int
         int test = 0;
         String displaystring = "";
 
-        for(int i = 1; i <= antalrader; i++){
+        for (int i = 1; i <= antalrader; i++) {
             //går igenom alla rader
-            for(int n = 1; n<=i; n++) {
+            for (int n = 1; n <= i; n++) {
                 //tar raden gånger i om i är mindre är lika med raden
-                int displaynumbs = i*n;
+                int displaynumbs = i * n;
                 //lägger till raden i stringen
                 displaystring = displaystring + Integer.toString(displaynumbs) + " ";
             }
@@ -27,26 +27,33 @@ public class Main {
 
         }
         JOptionPane.showMessageDialog(null, displaystring);
-    }
 
+    }
 
     public static void del2() {
         //sida 122, 5.7
-
-        //tror den inte fungerar
         double x = 0;
-        double y = 0;
-        for(int i = 2; x!=0; i++) {
-            x = 1/i;
-             y = y + x;
-            if(y > 0.0001) {
-                i = 0;
+        double i = 1;
+        double eivind = 1;
+
+        while(eivind/i >  0.00001) {
+            x += eivind/i;
+            i++;
+            x -= eivind/i;
+            i++;
+
+
             }
 
-            System.out.print(y);
-        }
+        System.out.print("ett delat på " + x +" är mindre än 0.00001");
+       }
 
-    }
+
+
+
+
+
+
 
 
 
